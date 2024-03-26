7c9a470a22050c32537048bf3b54e79adc8f7379
Mason Roberts <mrobe18@gmail.com>
2024-03-26 18:11:32 -0400

    playing with navbar

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Dropdown menu controller
+export default class extends Controller {
+    static targets = ["button", "menu"];
+
+    initialize() {
+        this.close();
+        this.closeMenuOnOutsideClick = this.closeMenuOnOutsideClick.bind(this);
+    }
+
+    connect() {
+        window.addEventListener("click", this.closeMenuOnOutsideClick);
+    }
+
+    disconnect() {
+        window.removeEventListener("click", this.closeMenuOnOutsideClick);
+    }
+
+    toggle() {
+        this.menuTarget.hidden === true ? this.open() : this.close();
+    }
+
+    open() {
+        this.menuTarget.hidden = false;
+        this.menuTarget.setAttribute("aria-hidden", "false");
+        this.buttonTarget.setAttribute("aria-expanded", "true");
+    }
+
+    close() {
+        this.menuTarget.hidden = true;
+        this.menuTarget.setAttribute("aria-hidden", "true");
+        this.menuTarget.setAttribute("aria-expanded", "false");
+    }
+
+    closeMenuOnOutsideClick(event) {
+        if (this.element === event.target || this.element.contains(event.target)) {
+            return;
+        }
+        this.close();
+    }
+}
