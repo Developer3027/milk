368674239abf073593a48eb2c1f6d82ef6d37f8a
Mason Roberts <mrobe18@gmail.com>
2024-05-12 22:03:36 -0400

    redesign on main devise forms

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  important: true,
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
